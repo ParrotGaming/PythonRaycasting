08178cb55c5bb13aab8d2ecdaae77749e0529194
Parrot <jacobparrotoffical@gmail.com>
2020-07-15 18:02:40 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+from ray import Ray
+from player import Player
+from math import *
+import pygame
+
+wall_size = 50
+p = Player()
+
+lines = open('map.txt',"r").read().split("\n")
+grid = [[int(c) for c in line] for line in lines]
+
+W = wall_size * len(grid)
+H = wall_size * len(grid[0])
+
+p.x = W/2
+p.y = H/2
+
+print(grid)
+
+pygame.init()
+
+size = (500, 500)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("Python Raycaster")
+ 
+done = False
+ 
+clock = pygame.time.Clock()
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+ 
+    screen.fill((255,255,255))
+
+    p.angle += 0.1
+
+    for c in range(500):
+        a = p.angle - p.fov/2 + c * p.fov/500
+        x = p.x
+        y = p.y
+        #Wrap This In a Loop
+        while(0 <= x <= 400 and 0 <= y <= 400):
+            gx = x // wall_size
+            gy = y // wall_size
+            tile = grid[int(gx)][int(gy)]
+            if tile == 1:
+                pygame.draw.line(screen, (0,0,0), (c,0), (c,500), 1)
+                break
+            x += cos(a)
+            y += sin(a)
+
+    pygame.display.flip()
+    clock.tick(60)
+ 
+pygame.quit()
---
+++ ray.py
@@ -0,0 +1,5 @@
+class Ray:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.angle = 0
