7887067ae50254be206491e5a58e7d7fa2f70407
ParrotGaming <killian@onyxwear.com>
2020-07-19 05:38:39 +0000

    Refactoring the wall drawing code.

--- main.py
+++ main.py
@@ -6,16 +6,11 @@ import pygame
 wall_size = 50
 p = Player()
 
-lines = open('map.txt',"r").read().split("\n")
+lines = open('map.txt',"r").read()
 grid = [[int(c) for c in line] for line in lines]
-
-#W = wall_size * len(grid)
-#H = wall_size * len(grid[0])
 
 #p.x = W/2
 #p.y = H/2
-
-print(grid)
 
 pygame.init()
 
@@ -28,6 +23,15 @@ done = False
  
 clock = pygame.time.Clock()
 
+pos = 0
+for c in lines:
+  x = (pos)
+  y = (pos // 10)
+  print(x, y)
+  if c == "1":
+    pygame.draw.rect(screen,(255,255,255),(200,200,50,50))
+  pos += 1
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -35,24 +39,23 @@ while not done:
  
     screen.fill((0,0,0))
 
-    p.angle += 0.1
-
-    for c in range(60):
-        a = p.angle - p.fov/2 + c * p.fov/500
-        x = p.x
-        y = p.y
-        #Wrap This In a Loop
-        while(0 <= x <= 500 and 0 <= y <= 500):
-            gx = x // wall_size
-            gy = y // wall_size
-            tile = grid[int(gx)][int(gy)]
-            if tile == 1:
-                pygame.draw.line(screen, (255,255,255), (x,y), (x,500), 1)
-                break
-            x += cos(a)
-            y += sin(a)
+    # for c in range(500):
+    #     a = p.angle - p.fov/2 + c * p.fov/500
+    #     x = p.x
+    #     y = p.y
+    #     #Wrap This In a Loop
+    #     while(0 <= x <= 500 and 0 <= y <= 500):
+    #         gx = x // wall_size
+    #         gy = y // wall_size
+    #         tile = grid[int(gx)][int(gy)]
+    #         if tile == 1:
+    #             pygame.draw.line(screen, (255,255,255), (c,0), (c,500), 1)
+    #             break
+    #         x += cos(a)
+    #         y += sin(a)
 
     pygame.display.flip()
+    pygame.display.update()
     clock.tick(60)
  
 pygame.quit()
